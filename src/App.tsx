a770c856127bf884e2857027caaa041659c2ddc5
Matheus Madureira <matheus.madureira@g.globo>
2022-01-16 04:09:04 -0300

    chore:testing app layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import getTimeSeriesIntraday from "./utils/getTimeSeriesIntraday";
 import getQuoteEndpoint from "./utils/getQuoteEndpoint";
 import getCurrencyExchangeRate from "./utils/getCurrencyExchangeRate";
 import Firstload from '../src/ui-components/Firstload';
+import Appshell from '../src/ui-components/Appshell';
+import Container from '../src/ui-components/Container';
 
 export default function App() {
   async function teste(){
@@ -18,7 +20,20 @@ export default function App() {
   
   return (
     <>
-      <Firstload />
+      <Appshell>
+        <Container>
+            <h1>Trending Stock</h1>
+        </Container>
+
+        <Container>
+            <h1>Your Portfolio</h1>
+        </Container>
+
+        <Container>
+            <h1>Stock List</h1>
+        </Container>
+      </Appshell>
+      
     </>
   )
 }
