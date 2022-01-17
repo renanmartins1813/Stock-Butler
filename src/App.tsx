4a5bf5c86d7392329cf3b9a41de49502e8427ba0
Matheus Madureira <matheus.madureira@g.globo>
2022-01-16 22:45:34 -0300

    feat: testing layout on main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,52 @@
 import getTimeSeriesIntraday from "./utils/getTimeSeriesIntraday";
 import getQuoteEndpoint from "./utils/getQuoteEndpoint";
 import getCurrencyExchangeRate from "./utils/getCurrencyExchangeRate";
-import Firstload from '../src/ui-components/Firstload';
-import Appshell from '../src/ui-components/Appshell';
-import Container from '../src/ui-components/Container';
+import Firstload from "../src/ui-components/Firstload";
+import Appshell from "../src/ui-components/Appshell";
+import Container from "../src/ui-components/Container";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  async function teste(){
-    await console.log(getTimeSeriesIntraday('AAPL', '1min'));
+
+  const [loading, setLoading] = useState(true)
+
+  async function teste() {
+    return await getTimeSeriesIntraday("AAPL", "1min")
   }
 
-  async function teste2(){
-    await console.log(getQuoteEndpoint('AAPL'));
+  async function teste2() {
+    console.log(await getQuoteEndpoint("AAPL"));
   }
 
-  async function teste3(){
-    await console.log(getCurrencyExchangeRate('USD', 'JPY'));
+  async function teste3() {
+    console.log(await getCurrencyExchangeRate("USD", "JPY"));
   }
-  
+
+  useEffect(()=>{
+    teste()
+    .then(()=> setLoading(false))
+    .catch(err => console.log(err.message))
+  }, [])
+
   return (
     <>
+      {loading? <Firstload/> : ''}
       <Appshell>
-        <Container>
-            <h1>Trending Stock</h1>
-        </Container>
+        <Container
+          title={"Trending Stock"}
+          icon={"bi bi-bar-chart-line-fill"}
+          subinfoIcon={"up"}
+          subinfoText={"AAPL 271,673 (+3,12%)"}
+          main={'Appl5, 304, 902'}
+        ></Container>
 
-        <Container>
-            <h1>Your Portfolio</h1>
-        </Container>
+        <Container
+          title={"Your Portfolio"}
+          icon={"bi bi-person-circle"}
+        ></Container>
 
-        <Container>
-            <h1>Stock List</h1>
-        </Container>
+        <Container title={"Stock List"}></Container>
       </Appshell>
-      
     </>
-  )
+  );
 }
